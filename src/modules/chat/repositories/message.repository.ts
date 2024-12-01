7e7cb3bcb00266648e33ab18274afa4da761d7b0
Jared Muñoz <amunozbaez669@gmail.com>
2024-11-30 18:56:57 -0600

    feat: find messages by both users

--- src/modules/chat/repositories/message.repository.ts
+++ src/modules/chat/repositories/message.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { eq, or } from 'drizzle-orm';
+import { and, eq, or } from 'drizzle-orm';
 import { db } from 'src/db/connection';
 import * as schema from 'src/db/schema';
 
@@ -16,6 +16,24 @@ export class MessageRepository {
                     eq(schema.messages.ownerId, userId),
                     eq(schema.messages.receptorId, userId),
                 ),
+            );
+    }
+
+    async findAllByReceptor(userId: string, receptorId: string) {
+        return await this.db
+            .select()
+            .from(schema.messages)
+            .where(
+                or(
+                    and(
+                        eq(schema.messages.ownerId, userId),
+                        eq(schema.messages.receptorId, receptorId),
+                    ),
+                    and(
+                        eq(schema.messages.ownerId, receptorId),
+                        eq(schema.messages.receptorId, userId),
+                    ),
+                )
             );
     }
 
