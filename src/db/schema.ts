022f6e9ae1bcf3cedb72394da188db2a62dee2be
Jared Muñoz <amunozbaez669@gmail.com>
2024-12-05 00:57:23 -0600

    fix: remove user row unused

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,7 +4,6 @@ import { relations } from 'drizzle-orm';
 export const users = pgTable('users', {
     id: uuid().defaultRandom().primaryKey(),
     name: text().notNull(),
-    // username: text().notNull().unique(),
     externalId: text().notNull()
 })
 
